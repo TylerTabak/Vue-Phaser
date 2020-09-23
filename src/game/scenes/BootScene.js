c4e31f8aff677e9ce803228d9a716fae9b3183a0
TylerTabak <tylertabak@hotmail.com>
2020-09-23 12:05:33 -0600

    Main Commit

--- src/game/scenes/BootScene.js
+++ src/game/scenes/BootScene.js
@@ -3,6 +3,10 @@ import sky from '@/game/assets/sky.png'
 import bomb from '@/game/assets/bomb.png'
 import thudMp3 from '@/game/assets/thud.mp3'
 import thudOgg from '@/game/assets/thud.ogg'
+import ground from '@/game/assets/platform.png'
+import star from '@/game/assets/star.png'
+import dude from '@/game/assets/dude.png'
+
 
 
 export default class BootScene extends Scene {
@@ -12,11 +16,14 @@ export default class BootScene extends Scene {
 
   preload () {
     this.load.image('sky', sky)
-    this.load.image('bomb', bomb)
-    this.load.audio('thud', [thudMp3, thudOgg])
+    this.load.image('ground', ground);
+    this.load.image('bomb', bomb);
+    this.load.image('star', star);
+    this.load.spritesheet('dude', dude, {frameWidth: 32, frameHeight: 48});
+    this.load.audio('thud', [thudMp3, thudOgg]);
   }
 
   create () {
-    this.scene.start('PlayScene')
+    this.scene.start('PlayScene');
   }
 }
--- src/game/scenes/PlayScene.js
+++ src/game/scenes/PlayScene.js
@@ -1,5 +1,8 @@
 import { Scene } from 'phaser'
 
+var platforms;
+var player;
+var bomb;
 
 export default class PlayScene extends Scene {
   constructor () {
@@ -7,14 +10,46 @@ export default class PlayScene extends Scene {
   }
 
   create () {
-    this.add.image(400, 300, 'sky')
+    this.add.image(400, 300, 'sky');
+    platforms = this.physics.add.staticGroup();
+    this.add.image(750, 50, 'star');
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+    bomb = this.physics.add.image(400, 200, 'bomb');
+    bomb.setCollideWorldBounds(true);
+    bomb.body.onWorldBounds = true; // enable worldbounds collision event
+    bomb.setBounce(1.1);
+    bomb.setVelocity(200, 20);
+    bomb.setMaxVelocity(700,700);
+    player = this.physics.add.sprite(100, 450, 'dude');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    player.body.setGravityY(300)
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(bomb, platforms);
 
-    const bomb = this.physics.add.image(400, 200, 'bomb')
-    bomb.setCollideWorldBounds(true)
-    bomb.body.onWorldBounds = true // enable worldbounds collision event
-    bomb.setBounce(1)
-    bomb.setVelocity(200, 20)
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
 
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.physics.add.overlap(player, bomb, resetPlayer, null, this);
     this.sound.add('thud')
     this.physics.world.on('worldbounds', () => {
       this.sound.play('thud', { volume: 0.75 })
@@ -22,5 +57,34 @@ export default class PlayScene extends Scene {
   }
 
   update () {
+    const cursors = this.input.keyboard.createCursorKeys();
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+    }
+
   }
+}
+function resetPlayer() {
+ player.x = 100;
+ player.y = 450;
 }
